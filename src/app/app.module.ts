48d0509755122c9bd36e866556a1db2f45219663
ahmedfarghaly261 <ahmedfarghaly201055@gmail.com>
2024-07-27 23:55:02 +0300

    push the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToDoListComponent } from './to-do-list/to-do-list.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToDoListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/to-do-list/to-do-list.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { from } from 'rxjs';
+
+@Component({
+  selector: 'app-to-do-list',
+  templateUrl: './to-do-list.component.html',
+  styleUrls: ['./to-do-list.component.css']
+})
+export class ToDoListComponent {
+
+  taskArray=[{taskName:'demo',done:false}]
+
+  OnAdd(TaskForm:NgForm) {
+    console.log(TaskForm)
+    this.taskArray.push({
+      taskName: TaskForm.controls['addTask'].value,
+      done:false
+    })   
+  }
+  
+  OnDelete(index :number){
+    console.log(index)
+    this.taskArray.splice(index,1)
+
+  }
+}
